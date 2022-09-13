c4978c9cb8dc41c4110cc7182e2e133683aaba8b
Michael Huang <michael@mayoduckpie.com>
2022-09-13 21:50:35 +0000

    Fulfill requirements of Assignment 1

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 #  coding: utf-8 
 import socketserver
 
@@ -26,21 +27,86 @@ import socketserver
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
+HOST, PORT = "localhost", 8080
+root_dir = 'www'
+
+mime_types = {
+        'html': 'text/html',
+        'css' : 'text/css',
+        'binary': 'application/octet-stream'
+}
+
 
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        request = parse_http_request(self.data.decode('utf-8'))
 
-if __name__ == "__main__":
-    HOST, PORT = "localhost", 8080
+        if request['method'].upper() != 'GET':
+            # TODO: Abstract with errno variable
+            self.request.sendall(bytearray('HTTP/1.1 405 Method Not Allowed', 'utf-8'))
+            return
 
+        header = 'HTTP/1.1 200 OK\nContent-Type: '
+        filetype = request['file'].split('.')[-1]
+        if not filetype.isalpha():  # Check if we should redirect the client
+            try:
+                host = request['host']
+            except KeyError:
+                host = f'{HOST}:{PORT}'
+
+            self.request.sendall(bytearray(f"HTTP/1.1 301 Moved Permanently\nLocation: http://{host}{request['file']}/", 'utf-8'))
+            return
+
+        if filetype == 'html':
+            header += mime_types['html']
+        elif filetype == 'css':
+            header += mime_types['css']
+        else:
+            header += mime_types['binary']
+
+        try:
+            requested_file = open(f"{root_dir}{request['file']}")
+        except FileNotFoundError:
+            self.request.sendall(bytearray('HTTP/1.1 404 Not Found', 'utf-8'))
+            return
+
+        self.request.sendall(bytearray('{}\n\n{}'.format(header, requested_file.read()), 'utf-8'))
+
+
+def parse_http_request(request_str: str):
+    # '\r\n' may be used for encoding newlines so remove '\r'
+    headers = request_str.replace('\r', '').split('\n')  
+
+    # We need only parse the first line since we are not expected to deal with
+    # request headers.
+    request = {}
+    method = headers[0].split(' ')
+    request['method'] = method[0]
+    request['file'] = method[1].replace('../', '')
+
+    if request['file'][-1] == '/':
+        request['file'] += 'index.html'
+
+    for header in headers[1:]:
+        if header.startswith('Host:'):
+            request['host'] = header.split(' ')[1]
+            break;
+
+    return request
+
+def main():
     socketserver.TCPServer.allow_reuse_address = True
     # Create the server, binding to localhost on port 8080
     server = socketserver.TCPServer((HOST, PORT), MyWebServer)
 
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
-    server.serve_forever()
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        return
+
+if __name__ == "__main__":
+    main()
